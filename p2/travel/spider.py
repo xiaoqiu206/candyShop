e76a23698e121f8919117b9145c256dfe735c8d0
xiaoq <xiaoq@DESKTOP-KCEN697>
2015-11-04 12:07:28 +0800

    backup

--- p2/travel/spider.py
+++
@@ -1,9 +0,0 @@
-# coding=utf-8
-'''
-Created on 2015年4月23日
-旅游信息的爬虫,直接用浏览器浏览
-@author: Administrator
-'''
-if __name__ == '__main__':
-    import utils
-    utils.getdata()
--- p2/travel/上传.bat
+++
@@ -1,1 +0,0 @@
-python upload2pgsql.pyc
